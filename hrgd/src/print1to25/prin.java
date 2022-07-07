3eb852d2f109d315f41db084ae8e9ad0afaa37fe
Admin <Admin@DESKTOP-C0O15PC>
2022-07-07 16:47:25 +0530

    tc

---
+++ hrgd/src/print1to25/prin.java
@@ -0,0 +1,13 @@
+package print1to25;
+
+public class prin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=25;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
